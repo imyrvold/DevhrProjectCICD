d75c13faabec4629902bb346de005088a4902bf3
Ivan C Myrvold <ivan@myrvold.org>
2021-03-21 20:26:56 +0100

    tries all records, not only the first one

--- Sources/RecognitionFunction/RekHandler.swift
+++ Sources/RecognitionFunction/RekHandler.swift
@@ -33,17 +33,63 @@ struct RekHandler: EventLoopLambdaHandler {
         }
         return context.eventLoop.makeSucceededFuture(())
     }
+    
+/*    func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
+        guard let record = event.records.first else { return context.eventLoop.makeFailedFuture(APIError.requestError) }
+        let db = DynamoDB(client: awsClient, region: .euwest1)
+        let rekognitionClient = Rekognition(client: awsClient)
+        
+        let safeKey = record.s3.object.key.replacingOccurrences(of: "%3A", with: ":")
+        let s3Object = Rekognition.S3Object(bucket: record.s3.bucket.name, name: safeKey)
+        let image = Rekognition.Image(s3Object: s3Object)
+        let detectLabelsRequest = Rekognition.DetectLabelsRequest(image: image, maxLabels: 10, minConfidence: minConfidence)
+        
+        let succeededFuture: EventLoopFuture<Out> = context.eventLoop.makeSucceededFuture(())
+        let futureRecords: [S3.Event.Record] = event.records
+        
+        
+        return futureRecords.map { record -> EventLoopFuture<Out> in
+            let safeKey = record.s3.object.key.replacingOccurrences(of: "%3A", with: ":")
+            let s3Object = Rekognition.S3Object(bucket: record.s3.bucket.name, name: safeKey)
+            let image = Rekognition.Image(s3Object: s3Object)
+            let detectLabelsRequest = Rekognition.DetectLabelsRequest(image: image, maxLabels: 10, minConfidence: minConfidence)
+
+            return rekognitionClient.detectLabels(detectLabelsRequest)
+                .flatMap { detectLabelsResponse -> EventLoopFuture<Void> in
+                    guard let rekLabels = detectLabelsResponse.labels,
+                          let imageLabelsTable = Lambda.env("TABLE") else {
+                        return context.eventLoop.makeSucceededFuture(())
+                    }
+
+                    // Instantiate a table resource object of our environment variable
+                    let labels = rekLabels.compactMap { $0.name }
+                    let rekEntry = RekEntry(image: safeKey, labels: labels)
+                    let putRequest = DynamoDB.PutItemCodableInput(item: rekEntry, tableName: imageLabelsTable)
+
+                    // Put item into table
+                    return db.putItem(putRequest)
+                        .flatMap { result in
+                            return context.eventLoop.makeSucceededFuture(())
+                        }
+                }
+        }
+    }*/
+
 
     func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
         guard let record = event.records.first else { return context.eventLoop.makeFailedFuture(APIError.requestError) }
-        let safeKey = record.s3.object.key.replacingOccurrences(of: "%3A", with: ":")
         let db = DynamoDB(client: awsClient, region: .euwest1)
+        let rekognitionClient = Rekognition(client: awsClient)
+        
+        let safeKey = record.s3.object.key.replacingOccurrences(of: "%3A", with: ":")
         let s3Object = Rekognition.S3Object(bucket: record.s3.bucket.name, name: safeKey)
         let image = Rekognition.Image(s3Object: s3Object)
-        let rekognitionClient = Rekognition(client: awsClient)
         let detectLabelsRequest = Rekognition.DetectLabelsRequest(image: image, maxLabels: 10, minConfidence: minConfidence)
+        
+        let succeededFuture: EventLoopFuture<Out> = context.eventLoop.makeSucceededFuture(())
 
-        return rekognitionClient.detectLabels(detectLabelsRequest)
+/*        let futureDetectLabels: EventLoopFuture<Rekognition.DetectLabelsResponse> = rekognitionClient.detectLabels(detectLabelsRequest)
+        let futureDetectLabelsResponse: EventLoopFuture<Out> = futureDetectLabels
             .flatMap { detectLabelsResponse -> EventLoopFuture<Void> in
                 guard let rekLabels = detectLabelsResponse.labels,
                       let imageLabelsTable = Lambda.env("TABLE") else {
@@ -56,11 +102,93 @@ struct RekHandler: EventLoopLambdaHandler {
                 let putRequest = DynamoDB.PutItemCodableInput(item: rekEntry, tableName: imageLabelsTable)
                 
                 // Put item into table
-                return db.putItem(putRequest)
+                let futurePutItemOutput/*: EventLoopFuture<DynamoDB.PutItemOutput>*/ = db.putItem(putRequest)
+                let futurePutItemOutputResponse: EventLoopFuture<Out> = futurePutItemOutput
                     .flatMap { result in
+                    return succeededFuture
+                }
+                
+                return futurePutItemOutputResponse
+            }
+        
+        return futureDetectLabelsResponse*/
+
+        
+        
+//        return futureDetectLabels
+//            .flatMap { detectLabelsResponse -> EventLoopFuture<Void> in
+//                guard let rekLabels = detectLabelsResponse.labels,
+//                      let imageLabelsTable = Lambda.env("TABLE") else {
+//                    return context.eventLoop.makeSucceededFuture(())
+//                }
+//
+//                // Instantiate a table resource object of our environment variable
+//                let labels = rekLabels.compactMap { $0.name }
+//                let rekEntry = RekEntry(image: safeKey, labels: labels)
+//                let putRequest = DynamoDB.PutItemCodableInput(item: rekEntry, tableName: imageLabelsTable)
+//
+//                // Put item into table
+//                let futurePutItemOutput/*: EventLoopFuture<DynamoDB.PutItemOutput>*/ = db.putItem(putRequest)
+//                let futurePutItemOutputResponse: EventLoopFuture<Out> = futurePutItemOutput
+//                    .flatMap { result in
+//                    return succeededFuture
+//                }
+//
+//                return futurePutItemOutputResponse
+//            }.map { _ in }
+
+        
+//        let futureRecords: [S3.Event.Record] = event.records
+//        let futureRecordsResult = futureRecords
+//            .flatMap { record in
+//                guard let rekLabels = detectLabelsResponse.labels, let imageLabelsTable = Lambda.env("TABLE") else { return succeededFuture }
+//
+//                // Instantiate a table resource object of our environment variable
+//                let labels = rekLabels.compactMap { $0.name }
+//                let rekEntry = RekEntry(image: safeKey, labels: labels)
+//                let putRequest = DynamoDB.PutItemCodableInput(item: rekEntry, tableName: imageLabelsTable)
+//
+//                // Put item into table
+//                return db.putItem(putRequest)
+//                    .flatMap { result in
+//                        return context.eventLoop.makeSucceededFuture(())
+//                    }
+//
+//            }
+        
+        let futureRecords: [S3.Event.Record] = event.records
+//
+//        EventLoopFuture<Void>.andAllSucceed(futureRecords, on: context.eventLoop)
+        
+
+        let futureRecordsResult = futureRecords.map { record -> EventLoopFuture<Out> in
+            let safeKey = record.s3.object.key.replacingOccurrences(of: "%3A", with: ":")
+            let s3Object = Rekognition.S3Object(bucket: record.s3.bucket.name, name: safeKey)
+            let image = Rekognition.Image(s3Object: s3Object)
+            let detectLabelsRequest = Rekognition.DetectLabelsRequest(image: image, maxLabels: 10, minConfidence: minConfidence)
+
+            return rekognitionClient.detectLabels(detectLabelsRequest)
+                .flatMap { detectLabelsResponse -> EventLoopFuture<Void> in
+                    guard let rekLabels = detectLabelsResponse.labels,
+                          let imageLabelsTable = Lambda.env("TABLE") else {
                         return context.eventLoop.makeSucceededFuture(())
                     }
-            }.map { _ in }
+
+                    // Instantiate a table resource object of our environment variable
+                    let labels = rekLabels.compactMap { $0.name }
+                    let rekEntry = RekEntry(image: safeKey, labels: labels)
+                    let putRequest = DynamoDB.PutItemCodableInput(item: rekEntry, tableName: imageLabelsTable)
+
+                    // Put item into table
+                    return db.putItem(putRequest)
+                        .flatMap { result in
+                            return context.eventLoop.makeSucceededFuture(())
+                        }
+                }
+        }
+        
+        return EventLoopFuture<Out>.andAllSucceed(futureRecordsResult, on: context.eventLoop)
+        
     }
         
 }
