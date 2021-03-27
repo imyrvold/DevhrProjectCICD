2bc2484dbc616bd88ed903b9ee57cffd48c236a3
Ivan C Myrvold <ivan@myrvold.org>
2021-03-27 12:53:10 +0100

    set a name for the image bucket

--- cdk/lib/devhr-project-stack.ts
+++ cdk/lib/devhr-project-stack.ts
@@ -18,6 +18,7 @@ export class DevhrProjectStack extends cdk.Stack {
     // Image Bucket
     // =================================================================================
     const imageBucket = new s3.Bucket(this, imageBucketName, {
+      bucketName: 'photoBucket',
       removalPolicy: cdk.RemovalPolicy.DESTROY
     })
     new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName })
