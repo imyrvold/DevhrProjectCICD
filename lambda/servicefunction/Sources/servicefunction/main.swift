3e0bf8ee12f97d725134865fc6870ee5efea283c
Ivan C Myrvold <ivan@myrvold.org>
2021-04-17 13:17:52 +0200

    debug with context.logger.info

--- lambda/servicefunction/Sources/servicefunction/main.swift
+++ lambda/servicefunction/Sources/servicefunction/main.swift
@@ -18,11 +18,17 @@ struct MyHandler {
 }
 
 Lambda.run { (context: Lambda.Context, event: APIGateway.Request, callback: @escaping (Result<APIGateway.Response, Error>) -> Void) in
+    context.logger.info("Lambda.run 1")
     guard
+        context.logger.info("Lambda.run 2")
         let bodyData = event.body?.data(using: .utf8),
+            context.logger.info("Lambda.run 3")
         let json = try? JSONSerialization.jsonObject(with: bodyData, options: []) as? [String: Any]
+    context.logger.info("Lambda.run 4")
     else {
+        context.logger.info("Lambda.run 5")
         callback(.success(APIGateway.Response(statusCode: .badRequest)))
+        context.logger.info("Lambda.run 6")
         return
     }
     
