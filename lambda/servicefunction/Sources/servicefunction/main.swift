e8bedfaf7469b19b82c8385934f5849c277142e5
Ivan C Myrvold <ivan@myrvold.org>
2021-04-17 12:55:47 +0200

    debug input

--- lambda/servicefunction/Sources/servicefunction/main.swift
+++ lambda/servicefunction/Sources/servicefunction/main.swift
@@ -1,4 +1,31 @@
 import AWSLambdaRuntime
+import AWSLambdaEvents
+import Foundation
 
-Lambda.run { ServiceHandler(context: $0) }
+protocol APIRequest {
+    var path: String { get }
+    var httpMethod: AWSLambdaEvents.HTTPMethod { get }
+    var queryStringParameters: [String: String]? { get }
+    var multiValueQueryStringParameters: [String: [String]]? { get }
+    var headers: AWSLambdaEvents.HTTPHeaders { get }
+    var multiValueHeaders: HTTPMultiValueHeaders { get }
+    var body: String? { get }
+    var isBase64Encoded: Bool { get }
+}
+
+struct MyHandler {
+    
+}
+
+Lambda.run { (context: Lambda.Context, event: APIGateway.Request, callback: @escaping (Result<APIGateway.Response, Error>) -> Void) in
+    guard
+        let bodyData = event.body?.data(using: .utf8),
+        let json = try? JSONSerialization.jsonObject(with: bodyData, options: []) as? [String: Any]
+    else {
+        callback(.success(APIGateway.Response(statusCode: .badRequest)))
+        return
+    }
+    
+    callback(.success(APIGateway.Response(statusCode: .ok, body: "fint")))
+}
 
