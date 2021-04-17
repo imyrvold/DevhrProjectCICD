53aab54f3709e65d2540930a369afd76099d8b18
Ivan C Myrvold <ivan@myrvold.org>
2021-04-17 10:39:59 +0200

    added apigateway constructs

--- cdk/lib/devhr-project-stack.ts
+++ cdk/lib/devhr-project-stack.ts
@@ -5,6 +5,8 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb'
 import { Duration } from '@aws-cdk/core'
 import * as iam from '@aws-cdk/aws-iam'
 import * as event_sources from '@aws-cdk/aws-lambda-event-sources'
+import * as apigw from '@aws-cdk/aws-apigateway'
+import { PassthroughBehavior } from '@aws-cdk/aws-apigateway'
 
 const imageBucketName = 'cdk-rekn-imagebucket'
 const resizedBucketName = imageBucketName + "-resized"
@@ -81,21 +83,101 @@ export class DevhrProjectStack extends cdk.Stack {
       timeout: Duration.seconds(30)
     });
 
-  // const serviceFn = new lambda.Function(this, 'serviceFunction', {
-  //   code: lambda.Code.fromAsset('servicelambda'),
-  //   runtime: lambda.Runtime.PYTHON_3_7,
-  //   handler: 'index.handler',
-  //   environment: {
-  //     "TABLE": table.tableName,
-  //     "BUCKET": imageBucket.bucketName,
-  //     "RESIZEDBUCKET": resizedBucket.bucketName
-  //   },
-  // });
-  ​
     imageBucket.grantWrite(serviceFn);
     resizedBucket.grantWrite(serviceFn);
     table.grantReadWriteData(serviceFn);
 
+    const api = new apigw.LambdaRestApi(this, 'imageAPI', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS
+      },
+      handler: serviceFn,
+      proxy: false
+    })
 
+    // =====================================================================================
+    // This construct builds a new Amazon API Gateway with AWS Lambda Integration
+    // =====================================================================================
+    const lambdaIntegration = new apigw.LambdaIntegration(serviceFn, {
+      proxy: false,
+      requestParameters: {
+        'integration.request.querystring.action': 'method.request.querystring.action',
+        'integration.request.querystring.key': 'method.request.querystring.key'
+      },
+      requestTemplates: {
+        'application/json': JSON.stringify({ action: "$util.escapeJavaScript($input.params('action'))", key: "$util.escapeJavaScript($input.params('key'))" })
+      },
+      passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            // We can map response parameters
+            // - Destination parameters (the key) are the response parameters (used in mappings)
+            // - Source parameters (the value) are the integration response parameters or expressions
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
+          }
+        },
+        {
+          // For errors, we check if the error message is not empty, get the error data
+          selectionPattern: "(\n|.)+",
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': "'*'"
+          }
+        }
+      ],
+    })
+
+    // =====================================================================================
+    // API Gateway
+    // =====================================================================================
+    const imageAPI = api.root.addResource('images')
+    ​
+    // GET /images
+    imageAPI.addMethod('GET', lambdaIntegration, {
+      requestParameters: {
+        'method.request.querystring.action': true,
+        'method.request.querystring.key': true
+      },
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        },
+        {
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        }
+      ]
+    })
+    
+    // DELETE /images
+    imageAPI.addMethod('DELETE', lambdaIntegration, {
+      requestParameters: {
+        'method.request.querystring.action': true,
+        'method.request.querystring.key': true
+      },
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        },
+        {
+          statusCode: "500",
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Origin': true,
+          },
+        }
+      ]
+    })
+    
   }
 }
